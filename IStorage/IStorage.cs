6d24c593e295738bd7f29103ed2151ebcf4f6fa6
Chengbo <Chengbo@DESKTOP-26EL3PU>
2017-02-22 18:27:08 -0500

    Added IStorage Interface

---
+++ IStorage/IStorage.cs
@@ -0,0 +1,18 @@
+﻿using PocketMusic.Storage.DataStorage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PocketMusic.Storage.DataStorage
+{
+    public interface IStorage
+    {
+        bool UpsertFileItem(FileItem fileItem);
+
+        bool DeleteFileItem(Guid id);
+
+        bool GetFileItem(Guid id);
+    }
+}
